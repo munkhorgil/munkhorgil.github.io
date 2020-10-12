887a3ad8efc752f3179afc86715cd7fcb900d8c6
Munkh-Orgil <munkhorgil@live.com>
2020-10-12 18:19:59 +0800

    Updates

---
+++ precache-manifest.aa77d0baab9310092403495ee2ff6819.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71dbc267625884e290b78a6a2ff857c9",
+    "url": "/munkhorgil.github.io/index.html"
+  },
+  {
+    "revision": "ae38ddd3527fdac41506",
+    "url": "/munkhorgil.github.io/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6079b4c17542216660e3",
+    "url": "/munkhorgil.github.io/static/css/main.bed489ab.chunk.css"
+  },
+  {
+    "revision": "ae38ddd3527fdac41506",
+    "url": "/munkhorgil.github.io/static/js/2.3342271c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/munkhorgil.github.io/static/js/2.3342271c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6079b4c17542216660e3",
+    "url": "/munkhorgil.github.io/static/js/main.44d40ea0.chunk.js"
+  },
+  {
+    "revision": "63e839f5bf480d947a20",
+    "url": "/munkhorgil.github.io/static/js/runtime-main.179fba00.js"
+  }
+]);
